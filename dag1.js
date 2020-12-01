a166c1dac61447fc714ad16a01104e00b2b69598
Gustav Andreasson <gustav.andreasson@gmail.com>
2020-12-01 16:23:29 +0100

    dag 1

--- dag1.js
+++ dag1.js
@@ -4,4 +4,4 @@ let a = document.getElementsByTagName("pre")[0].innerText.split("\n").slice(0,-1
 for(i=0;i<a.length;i++){b=a.slice(i).find(v=>v+a[i]==2020);if(b>=0)console.log(b*a[i])}
 
 //del 2
-for(i=0;i<a.length;i++)for(j=0;j<a.length;j++){b=a.slice(j).find(v=>v+a[i]+a[j]==2020);if(b>=0)console.log(b*a[i]*a[j])}
+for(i=0;i<a.length;i++)for(j=i;j<a.length;j++){b=a.slice(j).find(v=>v+a[i]+a[j]==2020);if(b>=0)console.log(b*a[i]*a[j])}
