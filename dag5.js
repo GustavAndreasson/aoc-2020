e2a25bbdd13b34366f5c939db103253ee6e776e0
Gustav Andreasson <gustav.andreasson@gmail.com>
2020-12-05 12:31:17 +0100

    förbättring dag 5

--- dag5.js
+++ dag5.js
@@ -1,5 +1,5 @@
 // del 1
-document.body.innerText.trim().split("\n").map(r=>parseInt(r.substr(0,7).replaceAll("B","1").replaceAll("F","0"),2)*8+parseInt(r.substr(7,3).replaceAll("R","1").replaceAll("L","0"),2)).reduce((m,v)=>m>v?m:v)
+document.body.innerText.trim().split("\n").map(r=>parseInt(r.replace(/B|R/g,"1").replace(/F|L/g,"0"),2)).reduce((m,v)=>m>v?m:v)
 
 // del 2
-document.body.innerText.trim().split("\n").map(r=>parseInt(r.substr(0,7).replaceAll("B","1").replaceAll("F","0"),2)*8+parseInt(r.substr(7,3).replaceAll("R","1").replaceAll("L","0"),2)).sort((a,b)=>a>b?1:-1).filter((v,i,a)=>v!=a[i-1]+1)[1]
+document.body.innerText.trim().split("\n").map(r=>parseInt(r.replace(/B|R/g,"1").replace(/F|L/g,"0"),2)).sort((a,b)=>a>b?1:-1).filter((v,i,a)=>v!=a[i-1]+1)[1]-1
