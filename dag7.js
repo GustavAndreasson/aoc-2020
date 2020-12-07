252f90aaf07e06a53de6c6a013434941e5402a90
Gustav Andreasson <gustav.andreasson@gmail.com>
2020-12-07 12:26:24 +0100

    dag 7 update

--- dag7.js
+++ dag7.js
@@ -1,9 +1,9 @@
 // del 1
 let a={}
-document.body.innerText.trim().split("\n").forEach(r=>a[r.split(" contain ")[0].split(" bag")[0]]=r.split(" contain ")[1].split(", ").map(c=>c=="no other bags."?null:[parseInt(c.substr(0,1)), c.substr(2).split(" bag")[0]]))
-let c=b=>b==="shiny gold"?true:(a[b][0]?a[b].reduce((r,d)=>r||c(d[1]),false):false)
-Object.keys(a).filter(r=>c(r)).length-1
+document.body.innerText.trim().split("\n").forEach(r=>a[r.split(" contain ")[0].split(" bag")[0]]=r.includes("no other bags")?[]:r.split(" contain ")[1].split(", ").map(c=>[parseInt(c.substr(0,1)), c.substr(2).split(" bag")[0]]))
+let c=b=>b==="shiny gold"||a[b].reduce((r,d)=>r||c(d[1]),false)
+Object.keys(a).filter(c).length - 1
 
 // del 2
-let c2=b=>a[b][0]?1+a[b].reduce((r,d)=>r+(d[0]*c2(d[1])),0):1
+let c2=b=>1+a[b].reduce((r,d)=>r+(d[0]*c2(d[1])),0)
 c2("shiny gold")-1
