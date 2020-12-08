12e4f0d2527e1e6bd4e3bc5f090178e577b61de3
Gustav Andreasson <gustav.andreasson@gmail.com>
2020-12-08 09:07:55 +0100

    dag 8 update

--- dag8.js
+++ dag8.js
@@ -8,13 +8,7 @@ while(!v.includes(p)&&c<30000){v.push(p);cpu[prg[p][0]](prg[p][1]);c++}
 prg.forEach((r,i)=>{
     prg=document.body.innerText.trim().split("\n").map(r=>r.split(" ")).map(r=>[r[0],parseInt(r[1])])
     a=0,p=0,c=0,v=[]
-    if(r[0]=="jmp") {
-	prg[i][0]="nop"
-	while(!v.includes(p)&&p<prg.length&&c<30000){v.push(p);cpu[prg[p][0]](prg[p][1]);c++}
-	if(p>=prg.length)console.log(i+" "+a)
-    } else if(r[0]=="nop") {
-	prg[i][0]="jmp"
-	while(!v.includes(p)&&p<prg.length&&c<30000){v.push(p);cpu[prg[p][0]](prg[p][1]);c++}
-	if(p>=prg.length)console.log(i+" "+a)
-    }
+    prg[i][0]=prg[i][0]=="nop"?"jmp":prg[i][0]=="jmp"?"nop":prg[i][0]
+    while(!v.includes(p)&&p<prg.length&&c<30000){v.push(p);cpu[prg[p][0]](prg[p][1]);c++}	
+    if(p>=prg.length)throw(i+" "+a)
 })
